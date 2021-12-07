3f7e65e930144fb4fe8462caed34627b1878c57b
Adam Orczyk <adam.orczyk@gmail.com>
2021-12-07 21:18:22 +0100

    Tests

--- test.ts
+++ test.ts
@@ -1,3 +1,7 @@
+/**
+ * PF Receiver tests
+ */
+
 pfReceiver.connectIrReceiver(DigitalPin.P2)
 
 let counter = 0;
@@ -22,7 +26,9 @@ let counter = 0;
 //     basic.showNumber(counter)
 // })
 
-// --- onSpeedRCcommand ---
+// Test onSpeedRCcommand
+
+// Channel1
 
 pfReceiver.onSpeedRCcommand(PfReceiverChannel.Channel1, PfSpeedControl.RedIncrement, PfAction.Pressed, () => {
     counter += 1;
@@ -34,21 +40,9 @@ pfReceiver.onSpeedRCcommand(PfReceiverChannel.Channel1, PfSpeedControl.RedDecrem
     basic.showNumber(counter)
 })
 
-pfReceiver.onSpeedRCcommand(PfReceiverChannel.Channel2, PfSpeedControl.RedDecrement, PfAction.Pressed, () => {
-    basic.showIcon(IconNames.Happy)
-})
-
-pfReceiver.onSpeedRCcommand(PfReceiverChannel.Channel2, PfSpeedControl.RedDecrement, PfAction.Pressed, () => {
-    basic.showIcon(IconNames.Heart)
-})
-
 pfReceiver.onSpeedRCcommand(PfReceiverChannel.Channel1, PfSpeedControl.RedBrake, PfAction.Pressed, () => {
-    basic.showNumber(counter)
     counter = 0;
-    // basic.showNumber(counter)
-    // pfReceiver.stopRecord()
-    console.log(JSON.stringify(pfReceiver.getRecordedCommands()))
-    pfReceiver.startRecord([0])
+    basic.showNumber(counter)
 })
 
 pfReceiver.onSpeedRCcommand(PfReceiverChannel.Channel1, PfSpeedControl.BlueIncrement, PfAction.Pressed, () => {
@@ -66,7 +60,19 @@ pfReceiver.onSpeedRCcommand(PfReceiverChannel.Channel1, PfSpeedControl.BlueBrake
     basic.showNumber(counter)
 })
 
-// --- onRCcommand ---
+// Channel2
+
+pfReceiver.onSpeedRCcommand(PfReceiverChannel.Channel2, PfSpeedControl.RedDecrement, PfAction.Pressed, () => {
+    basic.showIcon(IconNames.Happy)
+})
+
+pfReceiver.onSpeedRCcommand(PfReceiverChannel.Channel2, PfSpeedControl.RedDecrement, PfAction.Pressed, () => {
+    basic.showIcon(IconNames.Heart)
+})
+
+// Test onRCcommand
+
+// Channel1
 
 pfReceiver.onRCcommand(PfReceiverChannel.Channel1, PfControl.Forward, PfControl.Float, PfAction.Pressed, () => {
     counter += 1;
@@ -77,24 +83,6 @@ pfReceiver.onRCcommand(PfReceiverChannel.Channel1, PfControl.Backward, PfControl
     counter -= 1;
     basic.showNumber(counter)
 })
-
-
-pfReceiver.onRCcommand(PfReceiverChannel.Channel2, PfControl.Forward, PfControl.Float, PfAction.Pressed, () => {
-    basic.showIcon(IconNames.SmallHeart)
-})
-
-pfReceiver.onRCcommand(PfReceiverChannel.Channel2, PfControl.Forward, PfControl.Float, PfAction.Released, () => {
-    basic.clearScreen()
-})
-
-pfReceiver.onRCcommand(PfReceiverChannel.Channel2, PfControl.Backward, PfControl.Float, PfAction.Pressed, () => {
-    basic.showIcon(IconNames.Heart)
-})
-
-pfReceiver.onRCcommand(PfReceiverChannel.Channel2, PfControl.Backward, PfControl.Float, PfAction.Released, () => {
-    basic.clearScreen()
-})
-
 
 pfReceiver.onRCcommand(PfReceiverChannel.Channel1, PfControl.Float, PfControl.Forward, PfAction.Pressed, () => {
     counter += 2;
@@ -128,7 +116,25 @@ pfReceiver.onRCcommand(PfReceiverChannel.Channel1, PfControl.Backward, PfControl
     basic.showArrow(4)
 })
 
+// Channel2
 
+pfReceiver.onRCcommand(PfReceiverChannel.Channel2, PfControl.Forward, PfControl.Float, PfAction.Pressed, () => {
+    basic.showIcon(IconNames.SmallHeart)
+})
+
+pfReceiver.onRCcommand(PfReceiverChannel.Channel2, PfControl.Forward, PfControl.Float, PfAction.Released, () => {
+    basic.clearScreen()
+})
+
+pfReceiver.onRCcommand(PfReceiverChannel.Channel2, PfControl.Backward, PfControl.Float, PfAction.Pressed, () => {
+    basic.showIcon(IconNames.Heart)
+})
+
+pfReceiver.onRCcommand(PfReceiverChannel.Channel2, PfControl.Backward, PfControl.Float, PfAction.Released, () => {
+    basic.clearScreen()
+})
+
+// Test: startRecord, stopRecord and getRecordedCommands
 
 input.onButtonPressed(Button.A, function () {
     pfReceiver.startRecord([0]);
